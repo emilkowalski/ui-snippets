a5d703fb2019b23c3c2740a010ba4cbe39e05c6c
Emil Kowalski <d254624@edu.rocwb.nl>
2021-06-20 22:15:17 +0200

    Add next seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { globalStyles } from "@styles/global";
 import "vercel-toast/dist/vercel-toast.css";
 import Head from "next/head";
+import { NextSeo } from "next-seo";
 
 import Header from "@components/Header";
 
@@ -10,15 +11,30 @@ function MyApp({ Component, pageProps }) {
         <>
             <Head>
                 <meta name="keywords" content="ui snippets, ui, snippets, css, ui components, stitches, scss, ui inspiration" />
-                <title>UI Snippets</title>
                 <link rel="shortcut icon" href="favicon.ico" />
                 <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
                 <meta name="msapplication-TileColor" content="#847EE2" />
                 <meta name="theme-color" content="#847EE2" />
                 <meta name="twitter:site" content="@emilkowalski_" />
                 <meta name="twitter:creator" content="@emilkowalski_" />
-                <meta name="twitter:card" content="thumbnail" />
+                <meta name="twitter:card" content="summary_large_image" />
             </Head>
+            <NextSeo
+                title="UI Snippets"
+                description="A collection of CSS effects made with Next.js."
+                openGraph={{
+                    type: "website",
+                    url: "https://ui-snippets.dev",
+                    title: "UI Snippets",
+                    description: "A collection of CSS effects made with Next.js.",
+                    images: [
+                        {
+                            url: "https://ui-snippets.dev/thumbnail.jpg",
+                            alt: "UI Snippets",
+                        },
+                    ],
+                }}
+            />
             <Header />
             <Component {...pageProps} />
         </>
