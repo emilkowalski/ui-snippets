b68abe43dab4cd7726898d272ccbb5872331c509
Emil Kowalski <d254624@edu.rocwb.nl>
2021-07-02 16:23:37 +0200

    Change footer link url

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true, // Make sure eslint picks up the config at the root of the directory
+  parserOptions: {
+    ecmaVersion: 2020, // Use the latest ecmascript standard
+    sourceType: 'module', // Allows using import/export statements
+    ecmaFeatures: {
+      jsx: true // Enable JSX since we're using React
+    }
+  },
+  settings: {
+    react: {
+      version: 'detect' // Automatically detect the react version
+    }
+  },
+  env: {
+    browser: true, // Enables browser globals like window and document
+    amd: true, // Enables require() and define() as global variables as per the amd spec.
+    node: true // Enables Node.js global variables and Node.js scoping.
+  },
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended' // Make this the last element so prettier config overrides other formatting rules
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Use our .prettierrc file as source
+    'react/react-in-jsx-scope': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton']
+      }
+    ]
+  }
+};
--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,33 +1,33 @@
-import React from "react";
-import { styled } from "stitches.config";
-import Separator from "@components/Separator";
-import Stack from "@components/Stack";
-import LinkPreview from "@components/LinkPreview";
+import React from 'react';
+import { styled } from 'stitches.config';
+import Separator from '@components/Separator';
+import Stack from '@components/Stack';
+import LinkPreview from '@components/LinkPreview';
 
 const Footer = () => {
-    return (
-        <footer>
-            <Stack>
-                <Separator />
-                <Paragraph>
-                    Built by <LinkPreview url="https://emilkowal.ski/">Emil</LinkPreview> 🌕
-                </Paragraph>
-            </Stack>
-        </footer>
-    );
+  return (
+    <footer>
+      <Stack>
+        <Separator />
+        <Paragraph>
+          Built by <LinkPreview url="https://twitter.com/emilkowalski_">Emil</LinkPreview> 🌕
+        </Paragraph>
+      </Stack>
+    </footer>
+  );
 };
 
-const Paragraph = styled("p", {
-    fontSize: 14,
-    color: "$gray",
-    display: "block",
-    textAlign: "center",
+const Paragraph = styled('p', {
+  fontSize: 14,
+  color: '$gray',
+  display: 'block',
+  textAlign: 'center'
 });
 
-const Link = styled("a", {
-    textDecoration: "none",
-    color: "$white",
-    display: "inline-block",
+const Link = styled('a', {
+  textDecoration: 'none',
+  color: '$white',
+  display: 'inline-block'
 });
 
 export default Footer;
