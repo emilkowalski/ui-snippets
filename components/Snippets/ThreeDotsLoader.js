936f5fd62cb98572ad293cd73cc7d2aa8be511f2
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-07-11 16:22:32 +0200

    add SCSS styling to ThreeDotsLoader

--- components/Snippets/ThreeDotsLoader.js
+++ components/Snippets/ThreeDotsLoader.js
@@ -121,4 +121,49 @@ const cssStyling = `
 }
 `;
 
-export { stitchesStyling, cssStyling, Component };
+const scssStyling = `
+.exampleClass {
+  position: relative;
+  width: 14px;
+  height: 14px;
+  border-radius: 50%;
+  background-color: white;
+  color: white;
+  animation: flashing 0.8s infinite linear alternate 0.4s;
+
+  &::before,
+  &::after {
+    content: '';
+    display: inline-block;
+    position: absolute;
+    top: 0;
+    width: 14px;
+    height: 14px;
+    border-radius: 50%;
+    background-color: white;
+    color: white;
+  }
+
+  &::before {
+    left: -21px;
+    animation: flashing 0.8s infinite alternate;
+  }
+
+  &::after {
+    left: 21px;
+    animation: flashing 0.8s infinite alternate 0.8s;
+  }
+}
+
+@keyframes flashing {
+  0% {
+    background-color: white;
+  }
+  50%,
+  100% {
+    background-color: rgba(255, 255, 255, 0.3);
+  }
+}
+`;
+
+export { stitchesStyling, cssStyling, scssStyling, Component };
