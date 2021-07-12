d94d2ad3c2bc51ef4f6e4067b2c1940ccc365184
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-07-12 14:42:15 +0200

    add SCSS styling to SkollButton

--- components/Snippets/SkollButton.js
+++ components/Snippets/SkollButton.js
@@ -204,4 +204,70 @@ const cssStyling = `
   
 `;
 
-export { stitchesStyling, Component };
+const scssStyling = `
+.exampleClass {
+    overflow: hidden;
+    border-radius: 50%;
+    color: white;
+    width: 80px;
+    height: 80px;
+    padding: 0;
+    cursor: pointer;
+    border: none;
+    position: relative;
+    display: inline-block;
+    background-color: black;
+    font: 1rem inherit;
+  
+    &::before {
+      content: '';
+      background: white;
+      width: 100%;
+      height: 0;
+      padding-bottom: 100%;
+      border-radius: 50%;
+      transform: translate3d(0, 0, 0);
+      transition: transform 0.3s;
+      transition-timing-function: cubic-bezier(0.7, 0, 0.2, 1);
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+  
+    &:hover::before {
+      transform: translate3d(0, 100%, 0);
+    }
+  
+    span {
+      display: block;
+      position: relative;
+    }
+  
+    & > span {
+      overflow: hidden;
+      mix-blend-mode: difference;
+    }
+  
+    &:hover > span > span {
+      animation: MoveUpInitial 0.2s forwards, MoveUpEnd 0.2s forwards 0.2s;
+    }
+  }
+  
+  @keyframes MoveUpInitial {
+    to {
+      transform: translate3d(0, -105%, 0);
+    }
+  }
+  
+  @keyframes MoveUpEnd {
+    from {
+      transform: translate3d(0, 100%, 0);
+    }
+    to {
+      transform: translate3d(0, 0, 0);
+    }
+  }
+  
+`;
+
+export { stitchesStyling, cssStyling, scssStyling, Component };
