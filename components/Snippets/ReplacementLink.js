aeb756b8880e17f9bfaee7c768b6dfc2749c9aa4
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-07-21 16:42:12 +0200

    add SCSS styling to ReplacementLink

--- components/Snippets/ReplacementLink.js
+++ components/Snippets/ReplacementLink.js
@@ -103,4 +103,38 @@ const cssStyling = `
   }
   `;
 
-export { stitchesStyling, cssStyling, Component };
+const scssStyling = `
+.exampleClass {
+    overflow: hidden;
+    position: relative;
+    cursor: pointer;
+  
+    span {
+      display: inline-block;
+      transition: transform 0.3s cubic-bezier(0.5, 0.5, 0.3, 1);
+    }
+  
+    &::after {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      left: 0;
+      top: 0;
+      content: attr(data-text);
+      transform-origin: 100% 50%;
+      transform: translate3d(150%, 0, 0);
+      transition: transform 0.3s cubic-bezier(0.5, 0.5, 0.3, 1);
+    }
+  
+    &:hover::before {
+      transform: translate3d(0, 0, 0);
+    }
+  
+    &:hover span {
+      transform: translate3d(-150%, 0, 0);
+    }
+  }
+  
+`;
+
+export { stitchesStyling, cssStyling, scssStyling, Component };
