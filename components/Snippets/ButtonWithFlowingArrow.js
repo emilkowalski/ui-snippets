29280969b324a64d518e266b3ff3f7baa6d4f673
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-07-27 14:11:04 +0200

    add CSS styling to ButtonWithFLowingArrow

--- components/Snippets/ButtonWithFlowingArrow.js
+++ components/Snippets/ButtonWithFlowingArrow.js
@@ -154,4 +154,70 @@ const exampleWrapper = styled('button', {
 });
 `;
 
-export { stitchesStyling, Component };
+const cssStyling = `
+.exampleClass {
+  position: relative;
+  padding: 12px 24px;
+  background-color: white;
+  color: black;
+  border-radius: 7px;
+  cursor: pointer;
+  border: none;
+  font-family: inherit;
+  font-size: 1rem;
+  overflow: hidden;
+  transition: color 0.2s;
+}
+
+.exampleClass::before,
+.exampleClass::after {
+  height: 100%;
+  top: 0;
+  left: 0;
+  position: absolute;
+  content: '';
+  background-color: hsl(244, 63%, 69%);
+}
+
+.exampleClass::before {
+  width: 135%;
+  clip-path: polygon(75% 0%, 100% 50%, 75% 100%, 0% 100%, 0% 0%);
+  transform: translate3d(-100%, 0, 0);
+}
+
+.exampleClass::after {
+  width: 105%;
+  transform: translate3d(100%, 0, 0);
+  transition: transform 0.3s cubic-bezier(0.7, 0, 0.2, 1);
+}
+
+.exampleClass:hover {
+  color: white;
+}
+
+.exampleClass:hover::before {
+  transform: translate3d(0, 0, 0);
+  transition: transform 0.3s cubic-bezier(0.7, 0, 0.2, 1);
+}
+
+.exampleClass:hover::after {
+  transform: translate3d(0, 0, 0);
+  transition: transform 0.01s 0.3s cubic-bezier(0.7, 0, 0.2, 1);
+}
+
+.exampleClass:hover span {
+  animation: MoveRightInitial 0.1s forwards, MoveRightEnd 0.3s forwards 0.2s;
+}
+
+.exampleClass > span {
+  overflow: hidden;
+}
+
+.exampleClass span {
+  display: block;
+  position: relative;
+  z-index: 1;
+}
+`;
+
+export { stitchesStyling, cssStyling, Component };
