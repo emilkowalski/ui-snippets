04aa458d2aa393742e7c6441ee01a3bbc49b2784
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-07-27 14:12:57 +0200

    add SCSS styling to ButtonWithFLowingArrow

--- components/Snippets/ButtonWithFlowingArrow.js
+++ components/Snippets/ButtonWithFlowingArrow.js
@@ -220,4 +220,70 @@ const cssStyling = `
 }
 `;
 
-export { stitchesStyling, cssStyling, Component };
+const scssStyling = `
+.exampleClass {
+  position: relative;
+  padding: 12px 24px;
+  background-color: white;
+  color: black;
+  border-radius: 7px;
+  cursor: pointer;
+  border: none;
+  font-family: inherit;
+  font-size: 1rem;
+  overflow: hidden;
+  transition: color 0.2s;
+
+  &::before,
+  &::after {
+    height: 100%;
+    top: 0;
+    left: 0;
+    position: absolute;
+    content: '';
+    background-color: hsl(244, 63%, 69%);
+  }
+
+  &::before {
+    width: 135%;
+    clip-path: polygon(75% 0%, 100% 50%, 75% 100%, 0% 100%, 0% 0%);
+    transform: translate3d(-100%, 0, 0);
+  }
+
+  &::after {
+    width: 105%;
+    transform: translate3d(100%, 0, 0);
+    transition: transform 0.3s cubic-bezier(0.7, 0, 0.2, 1);
+  }
+
+  &:hover {
+    color: white;
+
+    &::before {
+      transform: translate3d(0, 0, 0);
+      transition: transform 0.3s cubic-bezier(0.7, 0, 0.2, 1);
+    }
+
+    &::after {
+      transform: translate3d(0, 0, 0);
+      transition: transform 0.01s 0.3s cubic-bezier(0.7, 0, 0.2, 1);
+    }
+
+    span {
+      animation: MoveRightInitial 0.1s forwards, MoveRightEnd 0.3s forwards 0.2s;
+    }
+  }
+
+  & > span {
+    overflow: hidden;
+  }
+
+  span {
+    display: block;
+    position: relative;
+    z-index: 1;
+  }
+}
+`;
+
+export { stitchesStyling, cssStyling, scssStyling, Component };
